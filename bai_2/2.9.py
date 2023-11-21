c6e646116ef552e557026452be3ab1c5a91bd9fb
PenguinnDeveloper <tronghuanxxx@gmail.com>
2023-11-21 14:18:30 +0700

    update 2.9

--- bai_2/2.9.py
+++ bai_2/2.9.py
@@ -1,16 +1,15 @@
 # 2.9 trang 72
 
 cccdID = input('nhap so can cuoc cong dan : ')
-
-for char in cccdID:
-    if (char.isdigit()):
-        if(len(cccdID) >= 9) and len(cccdID) <= 11:
-            print(f'so chung minh cua ban la {cccdID}')
-            break
-        else:
-            print('so chung minh phai co do dai tu 9 den 11 ky tu')
-            break
-    else :
-        print('Gia tri nhap vao chua chuoi ki tu!, vui long nhap lai')
-        break
-    
+while(True):
+        
+    for char in cccdID:
+        if (char.isdigit()):
+            if(len(cccdID) >= 9) and len(cccdID) <= 11:
+                print(f'so chung minh cua ban la {cccdID}')
+                break
+            else:
+                print('so chung minh phai co do dai tu 9 den 11 ky tu')
+        else :
+            print('Gia tri nhap vao chua chuoi ki tu!, vui long nhap lai')
+    break        

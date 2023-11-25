0a95c4294951c7cb5db311431681fffbcbab887f
PenguinnDeveloper <tronghuanxxx@gmail.com>
2023-11-25 09:23:48 +0700

    fix bug 4.3

--- bai_4/4.3.py
+++ bai_4/4.3.py
@@ -3,7 +3,7 @@ k = float(input('Nhập số giây cộng thêm: '))
 
 mangThoiGian = thoiGianHienTai.split(':')
 
-tongSoGiay = int(mangThoiGian[0]) * 3600 int(mangThoiGian[1]) * 60 + int(mangThoiGian[2]) + k
+tongSoGiay = int(mangThoiGian[0]) * 3600 + int(mangThoiGian[1]) * 60 + int(mangThoiGian[2]) + k
 
 gioMoi = tongSoGiay // 3600
 phutMoi = (tongSoGiay - (gioMoi * 3600)) // 60

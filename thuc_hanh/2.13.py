19de7231a6becec2d074f7cc3cfac7d031a64378
PenguinnDeveloper <tronghuanxxx@gmail.com>
2023-11-29 14:10:34 +0700

    update2.13

--- thuc_hanh/2.13.py
+++ thuc_hanh/2.13.py
@@ -3,6 +3,13 @@ while (True):
     if (n.isdigit()):
         n = int(n)
         for i in range(1, n+1):
-            break
+            isPrime = True
+            for j in range (2, i):
+                if (i % j == 0):
+                    isPrime = False
+                    break
+            if (isPrime == True):
+                print(f'{i} là số nguyên tố')
+        break
     else:
         print(f'{n} không phải là một số tự nhiên, Vui lòng nhập lại!')
